8fb7a59e4e1842828362b06e7e512c60c2518448
jeff martinez <jeffbmartinez@gmail.com>
2015-11-18 22:41:57 -0800

    Add user screenname at top of profile activity

--- app/src/main/java/com/codepath/apps/MySimpleTweets/activities/ProfileActivity.java
+++ app/src/main/java/com/codepath/apps/MySimpleTweets/activities/ProfileActivity.java
@@ -5,14 +5,32 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 import com.codepath.apps.MySimpleTweets.R;
+import com.codepath.apps.MySimpleTweets.TwitterApplication;
+import com.codepath.apps.MySimpleTweets.TwitterClient;
 import com.codepath.apps.MySimpleTweets.fragments.UserTimelineFragment;
+import com.codepath.apps.MySimpleTweets.models.User;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.json.JSONObject;
 
 public class ProfileActivity extends AppCompatActivity {
+    TwitterClient client;
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        client = TwitterApplication.getRestClient();
+        client.getUserInfo(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                user = User.fromJSON(response);
+                getSupportActionBar().setTitle("@" + user.getScreenName());
+            }
+        });
 
         String screenName = getIntent().getStringExtra("screenName");
 
