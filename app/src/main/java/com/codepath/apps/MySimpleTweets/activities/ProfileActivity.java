0cdb512fe961de081484afad86c3b2631e35869d
jeff martinez <jeffbmartinez@gmail.com>
2015-11-18 23:27:15 -0800

    Add profile details

--- app/src/main/java/com/codepath/apps/MySimpleTweets/activities/ProfileActivity.java
+++ app/src/main/java/com/codepath/apps/MySimpleTweets/activities/ProfileActivity.java
@@ -2,7 +2,10 @@ package com.codepath.apps.MySimpleTweets.activities;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.codepath.apps.MySimpleTweets.R;
 import com.codepath.apps.MySimpleTweets.TwitterApplication;
@@ -10,6 +13,7 @@ import com.codepath.apps.MySimpleTweets.TwitterClient;
 import com.codepath.apps.MySimpleTweets.fragments.UserTimelineFragment;
 import com.codepath.apps.MySimpleTweets.models.User;
 import com.loopj.android.http.JsonHttpResponseHandler;
+import com.squareup.picasso.Picasso;
 
 import org.apache.http.Header;
 import org.json.JSONObject;
@@ -28,7 +32,13 @@ public class ProfileActivity extends AppCompatActivity {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 user = User.fromJSON(response);
-                getSupportActionBar().setTitle("@" + user.getScreenName());
+
+                ActionBar actionBar = getSupportActionBar();
+                if (actionBar != null) {
+                    actionBar.setTitle("@" + user.getScreenName());
+                }
+
+                populateProfileHeader(user);
             }
         });
 
@@ -41,5 +51,25 @@ public class ProfileActivity extends AppCompatActivity {
             ft.replace(R.id.flContainer, fragmentUserTimeline);
             ft.commit();
         }
+    }
+
+    private void populateProfileHeader(User user) {
+        TextView tvName = (TextView) findViewById(R.id.tvName);
+        tvName.setText(user.getName());
+
+        TextView tvTagline = (TextView) findViewById(R.id.tvTagline);
+        tvTagline.setText(user.getDescription());
+
+        TextView tvFollowers = (TextView) findViewById(R.id.tvFollowers);
+        tvFollowers.setText("" + user.getFollowers() + " followers");
+
+        TextView tvFollowing = (TextView) findViewById(R.id.tvFollowing);
+        tvFollowing.setText("" + user.getFollowing() + " following");
+
+        TextView tvTweetsCount = (TextView) findViewById(R.id.tvTweetsCount);
+        tvTweetsCount.setText("" + user.getTweetsCount() + " tweets");
+
+        ImageView ivProfileImage = (ImageView) findViewById(R.id.ivProfileImage);
+        Picasso.with(this).load(user.getProfileImageUrl()).into(ivProfileImage);
     }
 }
--- app/src/main/java/com/codepath/apps/MySimpleTweets/models/User.java
+++ app/src/main/java/com/codepath/apps/MySimpleTweets/models/User.java
@@ -9,7 +9,11 @@ public class User implements Serializable {
     private String name;
     private long uid;
     private String screenName;
+    private String description;
     private String profileImageUrl;
+    private int followers;
+    private int following;
+    private int tweetsCount;
 
     public static User fromJSON(JSONObject json) {
         User u = new User();
@@ -18,7 +22,27 @@ public class User implements Serializable {
             u.name = json.getString("name");
             u.uid = json.getLong("id");
             u.screenName = json.getString("screen_name");
+            u.description = json.getString("description");
             u.profileImageUrl = json.getString("profile_image_url");
+
+            try {
+                u.followers = json.getInt("followers_count");
+            } catch (Exception e) {
+                u.followers = 0;
+            }
+
+            try {
+                u.following = json.getInt("friends_count");
+            } catch (Exception e) {
+                u.following = 0;
+            }
+
+            try {
+                u.tweetsCount = json.getInt("statuses_count");
+            } catch (Exception e) {
+                u.tweetsCount = 0;
+            }
+
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -38,66 +62,23 @@ public class User implements Serializable {
         return screenName;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
     public String getProfileImageUrl() {
         return profileImageUrl;
     }
-}
 
-/*
-{
-      "name": "OAuth Dancer",
-      "profile_sidebar_fill_color": "DDEEF6",
-      "profile_background_tile": true,
-      "profile_sidebar_border_color": "C0DEED",
-      "profile_image_url": "http://a0.twimg.com/profile_images/730275945/oauth-dancer_normal.jpg",
-      "created_at": "Wed Mar 03 19:37:35 +0000 2010",
-      "location": "San Francisco, CA",
-      "follow_request_sent": false,
-      "id_str": "119476949",
-      "is_translator": false,
-      "profile_link_color": "0084B4",
-      "entities": {
-        "url": {
-          "urls": [
-            {
-              "expanded_url": null,
-              "url": "http://bit.ly/oauth-dancer",
-              "indices": [
-                0,
-                26
-              ],
-              "display_url": null
-            }
-          ]
-        },
-        "description": null
-      },
-      "default_profile": false,
-      "url": "http://bit.ly/oauth-dancer",
-      "contributors_enabled": false,
-      "favourites_count": 7,
-      "utc_offset": null,
-      "profile_image_url_https": "https://si0.twimg.com/profile_images/730275945/oauth-dancer_normal.jpg",
-      "id": 119476949,
-      "listed_count": 1,
-      "profile_use_background_image": true,
-      "profile_text_color": "333333",
-      "followers_count": 28,
-      "lang": "en",
-      "protected": false,
-      "geo_enabled": true,
-      "notifications": false,
-      "description": "",
-      "profile_background_color": "C0DEED",
-      "verified": false,
-      "time_zone": null,
-      "profile_background_image_url_https": "https://si0.twimg.com/profile_background_images/80151733/oauth-dance.png",
-      "statuses_count": 166,
-      "profile_background_image_url": "http://a0.twimg.com/profile_background_images/80151733/oauth-dance.png",
-      "default_profile_image": false,
-      "friends_count": 14,
-      "following": false,
-      "show_all_inline_media": false,
-      "screen_name": "oauth_dancer"
+    public int getFollowers() {
+        return followers;
     }
- */
+
+    public int getFollowing() {
+        return following;
+    }
+
+    public int getTweetsCount() {
+        return tweetsCount;
+    }
+}
